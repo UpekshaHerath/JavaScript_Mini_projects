ab6464c5a57a473f3212697f449a4055e60ccdac
UpekshaHerath <upekshadilshan000@gmail.com>
2021-12-20 10:57:34 -0800

    Added the peopleCounter

---
+++ peopleCounterApp/function.js
@@ -0,0 +1,25 @@
+var count = 0;
+var savingString = "Previous counts";
+
+const countingValue = document.getElementById('count');
+const previousEntries = document.getElementById('previousEntries');
+
+
+function increment() {
+    count++;
+    countingValue.innerText = count;
+}
+
+function decrement() {
+    if (count <= 0) {
+        countingValue.innerText = count;
+    } else {
+        count--;
+        countingValue.innerText = count;
+    }
+}    
+    
+function save() {
+    savingString += " - " + count;
+    previousEntries.innerText = savingString;
+}
